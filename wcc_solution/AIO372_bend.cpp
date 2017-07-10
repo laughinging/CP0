9b726a5a006388d60a15f633bd4436ca0126f0c4
wucucu <irge@sina.cn>
2017-07-10 21:05:18 +0800

    k AIO372 & revised .gitignore

--- wcc_solution/AIO372_bend.cpp
+++ wcc_solution/AIO372_bend.cpp
@@ -1,3 +1,5 @@
+// http://orac.amt.edu.au/cgi-bin/train/problem.pl?set=simple1&problemid=372
+
 // This website takes input-output from file instead
 // of standard input output like most competitive
 // programming websites. Hence, here's a skeleton code
@@ -8,17 +10,17 @@
 using namespace std;
 
 int area(int x1, int y1, int x2, int y2) {
-    return (x2 - x1) * (y2 - y1)
+    return (x2 - x1) * (y2 - y1);
 }
 
 int is_in(int a, int b, int m) {
-    return (a<=m)&&(m<=b)
+    return (a<=m)&&(m<=b);
 }
 
 
 int main() {
-    freopen("INPUT_FILE_NAME.txt", "r", stdin);
-    freopen("OUTPUT_FILE_NAME.txt", "w", stdout);
+    freopen("bendin.txt", "r", stdin);
+    freopen("bendout.txt", "w", stdout);
 
     /* ... your code here ... */
     /* ... cin, cout will work normally. example ... */
@@ -33,8 +35,18 @@ int main() {
     s = area(x1, y1, x2, y2) + area(x3, y3, x4, y4);
 
     int overlap_s;
+    overlap_s = 0;
 
     int ox1, ox2, oy1, oy2;
+    ox1 = max(x1, x3);
+    ox2 = min(x2, x4);
+    oy1 = max(y1, y3);
+    oy2 = min(y2, y4);
+    if ((ox1<ox2) && (oy1<oy2)) {
+        overlap_s = area(ox1, oy1, ox2, oy2);
+    }
+
+    cout << s - overlap_s << endl;
 
     return 0;
 }
